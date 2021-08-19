cfbae89712b2d5d87f665d7840a5383903e3552e
Kitti U <kittiu@ecosoft.co.th>
2021-08-19 13:12:59 +0700

    auditlog, add auditlog.log.line view

--- auditlog/models/log.py
+++ auditlog/models/log.py
@@ -43,3 +43,14 @@ class AuditlogLogLine(models.Model):
     field_name = fields.Char("Technical name", related='field_id.name')
     field_description = fields.Char(
         "Description", related='field_id.field_description')
+    # From log auditlog.log
+    name = fields.Char(related="log_id.name", store=True)
+    model_id = fields.Many2one(related="log_id.model_id", store=True)
+    model_name = fields.Char(related="log_id.model_name", store=True)
+    model_model = fields.Char(related="log_id.model_model", store=True)
+    res_id = fields.Integer(related="log_id.res_id", store=True)
+    user_id = fields.Many2one(related="log_id.user_id", store=True)
+    method = fields.Char(related="log_id.method", store=True)
+    http_session_id = fields.Many2one(related="log_id.http_session_id", store=True)
+    http_request_id = fields.Many2one(related="log_id.http_request_id", store=True)
+    log_type = fields.Selection(related="log_id.log_type", store=True)
--- auditlog/readme/CONTRIBUTORS.rst
+++ auditlog/readme/CONTRIBUTORS.rst
@@ -3,3 +3,4 @@
 * Holden Rehg <holdenrehg@gmail.com>
 * Bhavesh Odedra <bodedra@opensourceintegrators.com>
 * Hardik Suthar <hsuthar@opensourceintegrators.com>
+* Kitti U. <kittiu@ecosoft.co.th>
